876bf36cbfa8536b91a6d46ebfaf28261f3c3042
Khalil <khalil.oumimoun@gmail.com>
2018-11-18 22:03:52 -0600

    #4: app.js working

---
+++ app.js
@@ -0,0 +1,62 @@
+var userScore = 0;
+var cpuScore = 0;
+const userScore_span = document.getElementById("user-score");
+const cpuScore_span = document.getElementById("cpu-score");
+const scoreBoard_div = document.querySelector(".score-board");
+const paper_div = document.getElementById("squirtle");
+const rock_div = document.getElementById("charmander");
+const scissor_div = document.getElementById("bulbasaur");
+
+function getComputerChoice(){
+    var moves = Array('rock', 'paper', 'scissor');
+    return moves[Math.floor(Math.random()*moves.length)];
+}
+function win(userChoice){
+    userScore++;
+    userScore_span.innerHTML = userScore;
+    cpuScore_span.innerHTML = cpuScore;
+    document.getElementById(userChoice).classList.add('green-glow');
+}
+function lose(){
+    cpuScore++;
+    cpuScore_span.innerHTML = cpuScore;
+    userScore_span.innerHTML = userScore;
+}
+function draw(){
+    userScore_span.innerHTML = userScore;
+    cpuScore_span.innerHTML = cpuScore;
+}
+
+function game(userChoice){
+    const computerChoice = getComputerChoice();
+    switch (userChoice + computerChoice){
+        case "rockscissor":
+        case "paperrock":
+        case "scissorpaper":
+            win();
+            break;
+        case "rockpaper":
+        case "paperscissor":
+        case "scissorrock":  
+            lose();
+            break; 
+        case "rockrock":
+        case "paperpaper":
+        case "scissorscissor":  
+            draw();
+            break; 
+    }
+}
+
+function main(){
+    rock_div.addEventListener('click', function(){
+        game("rock");
+    })
+    paper_div.addEventListener('click', function(){
+        game("paper");
+    })
+    scissor_div.addEventListener('click', function(){
+        game("scissor");
+    })
+}
+main();
